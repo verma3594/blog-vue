d1ea4c52996b4602069eca25f9c9c64f763506ef
Vishal Verma <vishalverma@Vishals-MacBook-Air.local>
2022-04-01 17:35:07 +0530

    Add json and axios

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,14 @@
     <div>
       <b-navbar toggleable="lg" type="light" variant="light">
         <b-container>
-          <b-navbar-brand>NavBar</b-navbar-brand>
+          <b-navbar-brand>
+            <img alt="Vue logo" width="40" src="./assets/logo.png" />
+          </b-navbar-brand>
 
           <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-        <div>
-          <router-link to="/">Home</router-link>
-          <router-link to="/about">About</router-link>
+          <div>
+            <router-link to="/">Home</router-link>
+            <router-link to="/about">About</router-link>
           </div>
         </b-container>
       </b-navbar>
@@ -18,13 +20,13 @@
 </template>
 
 <style scoped>
-.navbar{
-  background-color: #563d7c  !important; 
+.navbar {
+  background-color: #563d7c !important;
 }
-.navbar-light .navbar-brand{
+.navbar-light .navbar-brand {
   color: #fff;
 }
-.navbar a{
+.navbar a {
   color: #fff;
   text-decoration: none;
   margin: 0 15px;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,15 @@
 <template>
-  <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+ <HomeContent />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import HomeContent from "../components/HomeContent.vue";
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    HelloWorld
-  }
-}
+    HomeContent,
+  },
+};
 </script>
